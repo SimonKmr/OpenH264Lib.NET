9ca47e977924bfc258c6f60db6d79b9695823d76
SimonKmr <41740705+SimonKmr@users.noreply.github.com>
2023-09-21 16:02:45 +0200

    changed to Console App

--- OpenH264Sample/Program.cs
+++ OpenH264Sample/Program.cs
@@ -6,17 +6,48 @@ using System.Windows.Forms;
 
 namespace OpenH264Sample
 {
-    static class Program
+    public class Program
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
+        private const string DllName = "openh264-2.3.1-win32.dll";
+
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            var path = "C:\\Users\\Simon\\Desktop\\test.avi";
+
+            var p = new Program();
+            p.H264DecodeAvi(path, 1920, 1080);
+        }
+
+        private unsafe void H264DecodeAvi(string path, int width, int height)
+        {
+            Console.WriteLine("Decoding");
+            // create decoder
+            var decoder = new OpenH264Lib.Decoder(DllName);
+
+            // open file
+            var aviFile = System.IO.File.OpenRead(path);
+            var riff = new RiffFile(aviFile);
+
+            //create enumerator
+            var frames = riff.Chunks.OfType<RiffChunk>().Where(x => x.FourCC == "00dc");
+            var enumerator = frames.GetEnumerator();
+
+            // decode frames
+            while (enumerator.MoveNext())
+            {
+                var chunk = enumerator.Current;
+                var frame = chunk.ReadToEnd();
+
+                var size = width * height * 3 / 2;
+                var bytes = decoder.Decode(frame, frame.Length);
+
+                if (bytes == null) continue;
+                var res = new byte[size];
+
+                for (int i = 0; i < size; i++)
+                    res[i] = bytes[i];
+            }
         }
     }
 }
